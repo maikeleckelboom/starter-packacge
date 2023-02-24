7ebb0152895c078bf8d8a6571307641ea806e420
Maikel Eckelboom <maikeleckelboom@outlook.com>
2023-02-24 20:34:15 +0100

    feat: added vitest and lint script

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export const starterPackage = () => {
+    const message = "Hello World from Starter Package";
+    console.log(message);
+    return {
+        message
+    };
+};
+
+
+export default starterPackage
